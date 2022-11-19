9d662393ff72f95278a444fbc759cbb2253e1bb4
Igor Michalak <git@igormichalak.com>
2022-11-19 14:41:19 +0100

    feat: add colors to the theme

--- frontend/src/utils/styled/theme.ts
+++ frontend/src/utils/styled/theme.ts
@@ -19,6 +19,12 @@ const colors = {
     'hsl(271, 95%, 78%)',
     'hsl(271, 100%, 92%)',
   ],
+  white: '#ffffff',
+  lightGray: '#f1f1f1',
+  gray: '#656565',
+  blue: '#0052a5',
+  lightBlue: '#006cd7',
+  red: '#d5233f',
 };
 
 const zIndexes = {
