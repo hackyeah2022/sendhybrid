23c7e7cce016d77992c732a0beeea5d6a8aabafa
Bartlomiej Drozd <mail@bartlomiejdrozd.pl>
2022-11-20 03:05:56 +0100

    Polish validations + complete API

--- backend/src/main/java/com/mf/hybridesender/controller/FileController.java
+++ backend/src/main/java/com/mf/hybridesender/controller/FileController.java
@@ -1,11 +1,12 @@
 package com.mf.hybridesender.controller;
 
-import com.mf.hybridesender.controller.model.FileModel;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.mf.hybridesender.db.Document;
 import com.mf.hybridesender.db.FileDB;
+import com.mf.hybridesender.repositories.DocumentRepository;
 import com.mf.hybridesender.repositories.FileRepository;
 import com.mf.hybridesender.services.PdfReader;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.*;
@@ -14,36 +15,60 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/files")
+@JsonSerialize
 public class FileController {
 
     @Autowired
     private FileRepository fileRepository;
+
+    @Autowired
+    private DocumentRepository documentRepository;
     @Autowired
     private PdfReader pdfReader;
 
     @PostMapping("/upload")
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
-        String message = "";
+    public Document uploadFile(@RequestParam("file") MultipartFile file) {
+        FileDB fileDB = null;
         try {
             String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-            FileDB fileDB = new FileDB(fileName, file.getContentType(), file.getBytes());
-            FileDB savedFile = fileRepository.save(fileDB);
-            pdfReader.checkIfPdf(savedFile.getId());
-            pdfReader.checkSignatures(savedFile.getId());
-            pdfReader.checkIfFontsEmbeded(savedFile.getId());
-            pdfReader.checkImagesRequirements(savedFile.getId());
-            pdfReader.checkIfFreeOfForms(savedFile.getId());
-            message = "Uploaded the file successfully: " + file.getOriginalFilename();
-            return ResponseEntity.status(HttpStatus.OK).body(message);
-        } catch (Exception e) {
-            message = "Could not upload the file: " + file.getOriginalFilename() + "!";
-            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
+            fileDB = new FileDB(fileName, file.getContentType(), file.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Could not load file", e);
         }
+        FileDB savedFile = fileRepository.save(fileDB);
+        boolean pdfValidationFailed = !pdfReader.checkIfPdf(savedFile.getId());
+        boolean signatureValidationFailed = !pdfReader.checkSignatures(savedFile.getId());
+        boolean fontsValidationFailed = !pdfReader.checkIfFontsEmbeded(savedFile.getId());
+        boolean imagesValidationFailed = !pdfReader.checkImagesRequirements(savedFile.getId());
+        boolean formsValidationFailed = !pdfReader.checkIfFreeOfForms(savedFile.getId());
+
+        boolean generalValidationFailed = pdfValidationFailed || signatureValidationFailed || fontsValidationFailed || imagesValidationFailed || formsValidationFailed;
+
+        Document document = new Document();
+        document.setValidationPdfFailed(pdfValidationFailed);
+        document.setValidationSignatureFailed(signatureValidationFailed);
+        document.setValidationFontsFailed(fontsValidationFailed);
+        document.setValidationCMYKFailed(imagesValidationFailed);
+        document.setValidationFormFieldsFailed(formsValidationFailed);
+        document.setValidationGeneralFailed(generalValidationFailed);
+
+        document.setCreated(new Date());
+
+        document.setOriginalFileId(savedFile.getId());
+
+        Document savedDocument = documentRepository.save(document);
+
+        //return new FileModel(savedFile.getId(), savedFile.getName(), savedFile.getType(), savedFile.getCaseNumber(), savedFile.getReceiverName(), savedFile.getReceiverSurname(), savedFile.getReceiverStreet(), savedFile.getReceiverHouseNumber(), savedFile.getReceiverFlatNumber(), savedFile.getReceiverCity(), savedFile.getReceiverZipcode(), savedFile.getSenderName(), savedFile.getSenderSurname(), savedFile.getSenderStreet(), savedFile.getSenderHouseNumber(), savedFile.getSenderFlatNumber(), savedFile.getSenderCity(), savedFile.getSenderZipcode(), savedFile.isValidationPdfFailed(), savedFile.isValidationSignatureFailed(), savedFile.isValidationFormFieldsFailed(), savedFile.isValidationCMYKFailed(), savedFile.isValidationFontsFailed(), savedFile.isValidationGeneralFailed());
+        savedFile.setData(new byte[0]);
+
+        return savedDocument;
     }
 
     @GetMapping
@@ -71,9 +96,10 @@ public class FileController {
     }
 
     @GetMapping("/details/{id}")
-    public FileModel getDetails(@PathVariable String id) {
+    public FileDB getDetails(@PathVariable String id) {
         FileDB fileDB = fileRepository.getById(id);
 
-        return new FileModel(fileDB.getId(), fileDB.getName(), fileDB.getType());
+        return fileDB;
+        //return new FileModel(fileDB.getId(), fileDB.getName(), fileDB.getType(), fileDB.getCaseNumber(), fileDB.getReceiverName(), fileDB.getReceiverSurname(), fileDB.getReceiverStreet(), fileDB.getReceiverHouseNumber(), fileDB.getReceiverFlatNumber(), fileDB.getReceiverCity(), fileDB.getReceiverZipcode(), fileDB.getSenderName(), fileDB.getSenderSurname(), fileDB.getSenderStreet(), fileDB.getSenderHouseNumber(), fileDB.getSenderFlatNumber(), fileDB.getSenderCity(), fileDB.getSenderZipcode(), fileDB.isValidationPdfFailed(), fileDB.isValidationSignatureFailed(), fileDB.isValidationFormFieldsFailed(), fileDB.isValidationCMYKFailed(), fileDB.isValidationFontsFailed(), fileDB.isValidationGeneralFailed());
     }
 }
--- backend/src/main/java/com/mf/hybridesender/repositories/DocumentRepository.java
+++ backend/src/main/java/com/mf/hybridesender/repositories/DocumentRepository.java
@@ -3,8 +3,14 @@ package com.mf.hybridesender.repositories;
 import com.mf.hybridesender.db.Document;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DocumentRepository extends CrudRepository<Document, Integer> {
-    Document findByName(String name);
+    Document findById(String id);
+
+    List<Document> findByCaseNumber(String caseNumber);
+
+    List<Document> findAllByOrderByIdDesc();
 
     Document save(Document document);
 }
